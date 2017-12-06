13cf2bef23fc02096014bf143f35e790c7bf8ee8
Massaki Archambault <archambault.massaki@gmail.com>
2017-12-05 23:07:05 -0500

    writeonly decorator

--- simplefuse/decorators.py
+++ simplefuse/decorators.py
@@ -25,3 +25,23 @@ def readonly(node_class):
             setattr(node_class, member, noop)
 
     return node_class
+
+def writeonly(node_class):
+    noop = None
+    if issubclass(node_class, Filesystem):
+        def noop(self, *args, **kwargs):
+            raise FuseOSError(EROFS)
+    elif issubclass(node_class, Node):
+        def noop(self, *args, **kwargs):
+            raise FuseOSError(EPERM)
+    else:
+        logger.warning("@writeonly decorator on an incompatiple class: %s", node_class)
+        return node_class
+
+    member_to_noop = ('chmod', 'chown', 'open', 'read', 'readdir')
+
+    for member in dir(node_class):
+        if callable(getattr(node_class, member)) and member in member_to_noop:
+            setattr(node_class, member, noop)
+
+    return node_class
